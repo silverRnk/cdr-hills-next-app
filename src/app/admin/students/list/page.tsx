18236cd57af1de8d4f6c108cf23b6cc85403b25a
patrick <patdi.bautista@gmail.com>
2023-12-12 12:51:10 +0800

    added pagerReducer

--- src/app/admin/students/list/page.tsx
+++ src/app/admin/students/list/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 // import StudentTable from '@/components/ui/StudentTable/StudentTable'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import styled from 'styled-components'
 
 import dynamic from 'next/dynamic'
@@ -8,6 +8,7 @@ import { StudentProfileShort } from '@/util/types'
 import { setInterval } from 'timers/promises'
 import { setTimeout } from 'timers'
 import { studentListShort } from '../data'
+import { Pagination } from '@mui/material'
  
 const StudentTable = dynamic(() => import('@/components/ui/StudentTable/StudentTable'), { ssr: false })
 
@@ -31,9 +32,71 @@ const Wrapper = styled.div`
   box-shadow: 0px 0px 5px gray;
 `
 
+const Title = styled.h1`
+  width: 100%;
+  font-size: 2rem;
+  font-weight: bold;
+  color: #14238a;
+  margin-bottom: 40px;
+`;
+
+const PagerContainer = styled.div`
+  height: 100px;
+  width: 100%;
+  margin-top: 10px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const pagerState = {
+  pageCount: 1,
+  pageNumber: 1,
+}
+
+type PagerState = typeof pagerState;
+
+function pageReducer(reducer: PagerState, action: {type: string, pageCount: number, pageNumber}): PagerState{
+  let newPage;
+
+  switch(action.type){
+    case "set_page_count":
+      return {
+        pageCount: action.pageCount,
+        pageNumber: reducer.pageNumber
+      }
+    case "go_to_page":
+      return {
+        pageCount: reducer.pageCount,
+        pageNumber: action.pageNumber
+      }
+    case "next_page":
+      if(reducer.pageNumber <= reducer.pageCount){
+        newPage = reducer.pageNumber + 1
+      }else{
+        newPage = reducer.pageNumber
+      }
+      return {
+        pageCount: reducer.pageCount,
+        pageNumber: newPage
+      }
+    case "prev_page":
+      if(reducer.pageNumber > 0){
+        newPage = reducer.pageNumber - 1
+      }else{
+        newPage = reducer.pageNumber
+      }
+      return {
+        pageCount: reducer.pageCount,
+        pageNumber: newPage
+      }
+    default:
+      throw("Action does not exist")
+  }
+}
+
 const StudentList = () => {
   const [studentList, setStudentList] = useState<StudentProfileShort[]>([])
-
+  const [pageState, setPageState] = useReducer(pageReducer, pagerState)
   useEffect(() => {
     let timer = setTimeout(() => {
       console.log('Hello timmer'),
@@ -45,7 +108,15 @@ const StudentList = () => {
   return (
     <Container>
     <Wrapper>
+        <Title>All Students</Title>
         <StudentTable data={studentList} onSelectRow={() => {}} isLoading={studentList.length == 0}/>
+        <PagerContainer>
+        <Pagination
+          count={1}
+          page={1}
+          onChange={() => {}}
+        />
+        </PagerContainer>
     </Wrapper>
     </Container>
   )
